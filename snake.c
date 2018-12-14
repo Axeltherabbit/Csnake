d07a8b057d8b3010bc6afc2ae34afb986ace21f9
Andrzej Urbaniak <10389492+aurb@users.noreply.github.com>
2018-12-14 14:32:19 +0100

    Doubled game resolution
    - snake and food are drawn with UNICODE blocks
    - each terminal character contains two fields on the game board

--- snake.c
+++ snake.c
@@ -1,12 +1,17 @@
 #include <ncurses.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <locale.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
 
-#define termclean() printf("\033[H\033[J")
+/* UNICODE blocks for block elements.
+   See https://en.wikipedia.org/wiki/Block_Elements */
+#define UPPER_BLOCK "\u2580"
+#define LOWER_BLOCK "\u2584"
+#define FULL_BLOCK "\u2588"
+
 struct snake
 {
 	int posx;
@@ -39,23 +44,22 @@ int main()
   SDL_Init(SDL_INIT_AUDIO);
 
   Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,2048);
-  
+
   Mix_Music * music = Mix_LoadMUS("music.wav"); //background music
   Mix_Chunk * snd = Mix_LoadWAV("suono.wav"); //eat sound
 
- 
-  Mix_PlayMusic(music,-1); 
-  //
+  Mix_PlayMusic(music,-1);
 
   setlocale(LC_ALL, "");
 
   initscr(); /* Start curses mode */
   getmaxyx(stdscr,row,col);
-  
+  row *= 2;
+
   struct posfood snake_food;
   FOODP food = &snake_food;
   newfoodpos(food);
- 
+
   //snake head
   SNAKE head=malloc(sizeof(struct snake));
   head -> posx=row/2;
@@ -64,35 +68,32 @@ int main()
   head -> back = NULL;
 
   start_color(); /* Start color*/
-  
-  //background color
+
+  //background pair
   init_pair(1, COLOR_BLACK, COLOR_BLACK);
-  //snake color
+  //snake and background pair
   init_pair(2, COLOR_GREEN, COLOR_BLACK);
-  //food color
+  //food and background pair
   init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 
   bkgd(COLOR_PAIR(1));
- 
+
   keypad(stdscr, TRUE);  /* abilita la mappatura della tastiera */
   noecho(); /* Don't echo() while we do getch */
-  
 
   int add = 0;
   int direction='w';
-    
-  draw(head, &add, food, snd); 
-  
+
+  draw(head, &add, food, snd);
+
   //keyboard and music threads
   pthread_t readkey, playsound;
-  
-  
+
   int alive = 1;
   while(alive)
-     {	  
-	  
+     {
       movepieces(head,add);
-	
+
       switch (direction)
 	{
 	  case 'w': case KEY_UP:
@@ -107,9 +108,8 @@ int main()
 	  case 's': case KEY_DOWN:
 	      head -> posx += 1;
 	      break;
-    	      		  
 	}
-     
+
      draw(head, &add, food, snd);
 
      refresh();
@@ -117,33 +117,30 @@ int main()
      alive = collision(head);
      if (stop)
 	{
-	 
 	 mvprintw(0,0,"press SPACE to resume the game");
  	 char c='a';
 	 while(c != ' ') c = getch();
 	 stop = 0;
 	}
-     usleep(1000/10*1000); 
-     
+     usleep(1000/10*1000);
      }
 
   //snake color
   init_pair(2, COLOR_RED, COLOR_BLACK);
-  
+
   draw(head, &add, food, snd);
   getch();
-  endwin();  
+  endwin();
   Mix_FreeMusic(music);
   Mix_FreeChunk(snd);
   Mix_CloseAudio();
   printf("\n\nscreen size %d X %d\n snake : %d\n\n",row,col,snakelen(head));
-  	
-  
+
 }
 
 
-void *getkey(void *p) 
-{   
+void *getkey(void *p)
+{
    waitchar = 0;
    int c = getch();
 
@@ -162,16 +159,16 @@ void *getkey(void *p)
      case 'd': case KEY_RIGHT:
 	  if (c == 'a' || c == KEY_LEFT) change = 0;
 	  break;
-   } 
+   }
 
    if (change)
-   	switch (c) 
-    	{ 
-		case 'w': case 'a': case 's': case 'd': 
-		case  KEY_LEFT: case  KEY_RIGHT:  case KEY_UP: case  KEY_DOWN:  		  
+   	switch (c)
+    	{
+		case 'w': case 'a': case 's': case 'd':
+		case  KEY_LEFT: case  KEY_RIGHT:  case KEY_UP: case  KEY_DOWN:
 		  *(int *) p = c;
 		  break;
-		
+
 		case ' ': //pause button
 		  stop = 1;
 		  break;
@@ -180,28 +177,25 @@ void *getkey(void *p)
 
 }
 
-
 void movepieces(SNAKE p, int add)
 {
   int oldposx, oldposy;
   SNAKE piece = p;
   SNAKE start = p;
-  
-  
+
   while(p -> next != NULL) p = p -> next;
   if (add)
   {
     oldposx = p -> posx;
     oldposy = p -> posy;
   }
-  while(p -> back != NULL) 
+  while(p -> back != NULL)
    {
      p -> posx = p -> back -> posx;
      p -> posy = p -> back -> posy;
      p = p -> back;
    }
-  
-  
+
   if (add)
   { //create a new piece
     while (p -> next != NULL) p = p -> next;
@@ -209,42 +203,51 @@ void movepieces(SNAKE p, int add)
     p -> next -> next = NULL;
     p -> next -> back = p;
     p -> next -> posx = oldposx;
-    p -> next -> posy = oldposy; 
+    p -> next -> posy = oldposy;
   }
-
 
 }
 
 void newfoodpos(FOODP food)
 {
  food -> posy = rand() % col;
- food -> posx = rand() % row; 
+ food -> posx = rand() % row;
 }
 
-
 void draw(SNAKE p, int *add, FOODP food, Mix_Chunk * snd)
-{ 
-  if (p -> posx == food -> posx && p -> posy == food -> posy) 
+{
+  if (p -> posx == food -> posx && p -> posy == food -> posy)
     {
       Mix_PlayChannel(-1,snd,0);
       newfoodpos(food);
       *add = 1;
     }
   else *add = 0;
-  
-  attron(COLOR_PAIR(1)); //BACKGROUND COLOR
-  clear(); //clear window	
-  attron(COLOR_PAIR(2)); //SNAKE COLOR
-  while (p -> next != NULL)
+
+  /* clear window */
+  attron(COLOR_PAIR(1));
+  clear();
+
+  /* draw snake */
+  attron(COLOR_PAIR(2));
+  while (p != NULL)
   {
-     mvprintw(p -> posx, p -> posy,"■");
-     p = p -> next;
+    if (p->next != NULL && (p->posx&3 ^ p->next->posx&3) == 1) {
+      mvprintw(p->posx>>1, p->posy, FULL_BLOCK);
+      p = p->next;
+    }
+    else {
+      mvprintw(p->posx>>1, p->posy, p->posx&1 ? LOWER_BLOCK : UPPER_BLOCK);
+    }
+     p = p->next;
   }
-  mvprintw(p -> posx, p -> posy,"■");
-  
-  attron(COLOR_PAIR(3)); //FOOD COLOR
-  mvprintw(food -> posx, food -> posy,"■");
-  move(row+1,col+1);
+
+  /* draw food */
+  attron(COLOR_PAIR(3));
+  mvprintw(food->posx>>1, food->posy, food->posx&1 ? LOWER_BLOCK : UPPER_BLOCK);
+
+  /* move cursor to the corner, to avoid interference with already drawn characters*/
+  move(row/2-1, col-1);
 }
 
 int collision(SNAKE p)
